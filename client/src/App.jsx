8f951c7a65230dd8801f5f167d4a920bc7efedca
suvssagrawal <suvssextras@gmail.com>
2026-02-15 14:23:13 +0530

    Made routing connections

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import Navbar from './components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './components/Home'
+import Movies from './components/Movies'
+import MovieDetails from './components/MovieDetails'
+import SeatLayout from './components/SeatLayout'
+import MyBookings from './components/MyBookings'
+import Favorite from './components/Favorite'
+import {Toaster} from 'react-hot-toast'
 const App = () => {
-  return (
-    <div>App</div>
+  const isAdminRoute = useLocation().pathname.startsWith('/admin');
+   return (
+    <>
+    <Toaster/>
+    {isAdminRoute ? null : <Navbar />}
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/movies' element={<Movies/>}/>
+      <Route path='/movies/:id' element={<MovieDetails/>}/>
+      <Route path='/movies/:id/:date' element={<SeatLayout/>}/>
+      <Route path='/my-bookings' element={<MyBookings/>}/>
+      <Route path='/favorite' element={<Favorite/>}/>
+    </Routes>
+    </>
   )
 }
 
