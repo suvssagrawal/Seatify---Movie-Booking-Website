254df988b31e6ad16a551902fddc7754da62afd7
suvssagrawal <suvssextras@gmail.com>
2026-02-22 02:13:00 +0530

    Header

---
+++ client/src/components/HeroSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+        <div className=' flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("backgroundImage.png")] '></div>
+  )
+}
+
+export default HeroSection
